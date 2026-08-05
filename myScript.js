ba87bd01494773dd0cc04159b029b772773651bb
Navanee <navaneenavanee512@gmail.com>
2026-08-05 14:31:22 +0530

    first commit

---
+++ myScript.js
@@ -0,0 +1,88 @@
+ function fetchCourses() {
+  fetch('http://localhost:8080/courses')
+            .then(response => response.json())
+            .then(data => {
+                const tbodyy = document.getElementById('courseTableBody');
+                data.forEach(course => {
+                    const row = `<tr>
+                        <td>${course.courseId}</td>
+                        <td>${course.courseName}</td>
+                        <td>${course.trainer}</td>
+                        <td>${course.durationInWeeks}</td>
+                    </tr>`
+                    tbodyy.innerHTML += row;
+                });
+            })
+            .catch(error => console.error('Error fetching courses:', error));
+        }
+        // Popup open function
+function openLogin() {
+    document.getElementById("loginPopup").style.display = "block";
+}
+
+// Popup close function
+function closeLogin() {
+    document.getElementById("loginPopup").style.display = "none";
+}
+function fetchEnrolledCourses() {
+
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    fetch("http://localhost:8080/admin/courses/enrolledStudents", {
+
+        method: "GET",
+
+        headers: {
+            Authorization: "Basic " + btoa(username + ":" + password)
+        }
+
+    })
+
+    .then(response => {
+
+        if(response.status==401){
+            throw new Error("Invalid Username or Password");
+        }
+
+        if(response.status==403){
+            throw new Error("Access Denied");
+        }
+
+        return response.json();
+
+    })
+
+    .then(data=>{
+
+        // Hide popup
+        document.getElementById("loginPopup").style.display="none";
+
+        // Show table
+        document.getElementById("studentTable").style.display="table";
+
+        const tbody=document.getElementById("enrolledCoursesBody");
+
+        tbody.innerHTML="";
+
+        data.forEach(student=>{
+
+            tbody.innerHTML += `
+                <tr>
+                    <td>${student.name}</td>
+                    <td>${student.emailId}</td>
+                    <td>${student.courseName}</td>
+                </tr>
+            `;
+
+        });
+
+    })
+
+    .catch(error=>{
+
+        alert(error.message);
+
+    });
+
+}
